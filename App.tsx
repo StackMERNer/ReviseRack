0502eb07f8332036c09f8196b483678ebe24c5f5
Md Ashraful Islam <ms785314@gmail.com>
2024-01-14 12:27:47 +0600

    install dependencis, implement pdf displaying functionality

--- App.tsx
+++ App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {Dimensions, StyleSheet, View} from 'react-native';
+import Pdf from 'react-native-pdf';
 
 function App() {
+  const pdfSource = 'https://api.printnode.com/static/test/pdf/multipage.pdf';
   return (
-    <SafeAreaView>
-      <View>
-        <Text>hello world</Text>
-        <Text>hello world</Text>
-        <Text>hello world</Text>
+    <>
+      <View style={styles.container}>
+        <Pdf
+          trustAllCerts={false}
+          source={{
+            uri: pdfSource,
+          }}
+          style={styles.pdf}
+        />
+        {/* <Text>hello</Text> */}
       </View>
-    </SafeAreaView>
+    </>
   );
 }
 
-export default App
+const styles = StyleSheet.create({
+  container: {flex: 1, justifyContent: 'flex-start', alignItems: 'center'},
+  pdf: {flex: 1, width: Dimensions.get('window').width},
+});
+
+export default App;
