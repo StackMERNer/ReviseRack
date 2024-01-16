42019ebfd76aacaf63c9b7bfbb772b5d7d763bac
Md Ashraful Islam <ms785314@gmail.com>
2024-01-16 19:51:40 +0600

    display folders

--- screens/FileManager.tsx
+++ screens/FileManager.tsx
@@ -1,16 +1,76 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  FlatList,
   Modal,
+  PermissionsAndroid,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
+import RNFS from 'react-native-fs';
 function FileManagement() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [currPath, setCurrPath] = useState(RNFS.DocumentDirectoryPath);
+  const [folders, setFolders] = useState([]);
+
+  const getAllFolders = () => {
+    RNFS.readDir(currPath) // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
+      .then(result => {
+        // console.log('GOT RESULT', result);
+        setFolders(result);
+
+        // stat the first file
+        // return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+      })
+      .catch(err => {
+        console.log(err.message, err.code);
+      });
+  };
+  const requestStoragePermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        {
+          title: 'Cool Photo App Storage Permission',
+          message:
+            'Cool Photo App needs access to your Storage ' +
+            'so you can take awesome pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      console.log(granted);
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        // console.log('You can use the Storage');
+        getAllFolders();
+      } else {
+        console.log('Storage permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  useEffect(() => {
+    requestStoragePermission();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <View style={styles.container}>
+      <View style={styles.folderContainer}>
+        <FlatList
+          data={folders}
+          numColumns={2}
+          //   contentContainerStyle={{columnGap: 5}}
+          renderItem={({item, index}) => (
+            <TouchableOpacity style={styles.folder}>
+              <Text>{item.name}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
       <TouchableOpacity
         onPress={() => setModalVisible(true)}
         style={styles.addButton}>
@@ -105,6 +165,22 @@ const styles = StyleSheet.create({
     width: '90%',
     marginTop: 30,
     alignContent: 'center',
+  },
+  folders: {
+    flexDirection: 'row',
+    flexShrink: 4,
+  },
+  folderContainer: {
+    marginTop: 20,
+  },
+  folder: {
+    height: 40,
+    width: '45%',
+    margin: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
